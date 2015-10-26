dc07a710be48c5717b417aa75fc5286e800425f9
Vladislav Khvostov <vladkhvo@gmail.com>
2015-10-26 10:01:39 +0300

    parse random the title

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var request = require('request');
 
 var options = {
-  url: 'https://a.wunderlist.com/api/v1/user',
+  url: 'https://a.wunderlist.com/api/v1/tasks?list_id=150029475',
   headers: {
     "X-Access-Token": "8d5596e05ed8de4c4eb5a9654fe5673c98b437f0d7cce23b730e97f0dbdb",
-    "X-Client-ID": "94a0ac48561078fd5530"
-  }
+    "X-Client-ID": "94a0ac48561078fd5530",
+  },
 };
 
 
 
 
 request(options, function (error, response, body) {
+    //console.log(error);
+
   if (!error && response.statusCode == 200) {
-    console.log(body)
+    console.log( JSON.parse(body)[0].title)
   }
 })
 
