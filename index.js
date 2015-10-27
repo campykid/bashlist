2df7e8b77e49bb6b24b7cd21d70457abd30a1b72
Vladislav Khvostov <vladkhvo@gmail.com>
2015-10-27 22:11:45 +0300

    changes:  used the arrow function expression.

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ bashlit.requestUrls = {
 	getRequestUrl: function (type, param) {
 		if (param) {
 			let url = 'https://' + this[type] + param;
-			return  url	
+			return url	
 		} else {
 			let url = 'https://' + this[type];
-			return  url;
+			return url;
 		}
 	}
 }
@@ -31,33 +31,33 @@ let listsUrl = bashlit.requestUrls.getRequestUrl('lists')
 
 // Тело запроса. like - https://a.wunderlist.com/api/v1/tasks?list_id=150029475
 let requesBody = {
-  url: listsUrl,
-  // Заголовки
-  headers: bashlit.authKeys 
+ url: listsUrl,
+ // Заголовки
+ headers: bashlit.authKeys 
 };
 
 
 
 // Шлем запрос к апи чтобы получить списки.
-request(requesBody, function (error, response, body) {
+request(requesBody, (error, response, body) => {
 
-  if (!error && response.statusCode == 200) {
+ if (!error && response.statusCode == 200) {
 
-  	// Парсим ответ, получаем массив из объетов-списков
-    let answerJSON = JSON.parse(body);
-    let allListsId = [];
-    let urlForReques = [];
+ 	// Парсим ответ, получаем массив из объетов-списков
+ let answerJSON = JSON.parse(body);
+ let allListsId = [];
+ let urlForReques = [];
 
 
-	let promise = new Promise(function(resolve, reject) {
-	    // Получаем массив с идентификаторами от каждого списка.
-	    answerJSON.forEach(function (item, index, array) {
-	    	allListsId.push(item.id);
-	    });
-	    resolve(allListsId);
+	let promise = new Promise((resolve, reject) => {
+	 // Получаем массив с идентификаторами от каждого списка.
+	 answerJSON.forEach((item, index, array) => {
+	 	allListsId.push(item.id);
+	 });
+	 resolve(allListsId);
 	})
 
-	promise.then(function (allListsId) {
+	promise.then((allListsId) => {
 		let tasksRequests = [];
 		
 		allListsId.forEach(function (elem) {
@@ -65,27 +65,26 @@ request(requesBody, function (error, response, body) {
 			tasksRequests.push(bashlit.requestUrls.getRequestUrl('tasks', elem));
 		})
 		return tasksRequests;
-	}).then(function (tasksRequests) {
-			//
-			let requesBody = {};
-			// Добавляем в тело зпроса штуки для авторизации.
-			requesBody.headers = bashlit.authKeys;
-			
-			tasksRequests.forEach(function (link) {
-				 // Добавляем в тело зпроса урл
-				 requesBody.url = link;
-				 // Делам запрос для каждой ссылки.
-				 request(requesBody, function (error, response, body) {
-				 	let responseJSON = JSON.parse(body);
-				 	responseJSON.forEach(function (elem) {
-				 		// Выводим в консоль все таски.
-				 		process.stdout.write(elem.title + '\n');
-				 	})
-				 })
-				 
+	}).then((tasksRequests) => {
+		//
+		let requesBody = {};
+		// Добавляем в тело зпроса штуки для авторизации.
+		requesBody.headers = bashlit.authKeys;
+		
+		tasksRequests.forEach((link) => {
+			 // Добавляем в тело зпроса урл
+			 requesBody.url = link;
+			 // Делам запрос для каждой ссылки.
+			 request(requesBody, (error, response, body) => {
+				let responseJSON = JSON.parse(body);
+			 	responseJSON.forEach((elem) => {
+			 		// Выводим в консоль все таски.
+			 		process.stdout.write(elem.title + '\n');
+			 	})
 			})
+		})
 	})
-  }
+ }
 });
 
 
