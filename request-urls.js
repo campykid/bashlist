25573293f54a1f18b5741feead75957e57e87e89
Владислав Хвостов <vladkhvo@gmail.com>
2015-10-30 09:44:41 +0300

    Added the jsdoc

--- request-urls.js
+++ request-urls.js
@@ -4,6 +4,12 @@ let requestUrls = {
 	tasks: 'a.wunderlist.com/api/v1/tasks?list_id=',
 	lists: 'a.wunderlist.com/api/v1/lists',
 
+	/**
+	 * Returns urls with the necessary get parameters .
+	 * 
+	 * @param {string} type - Type of the request.
+	 * @param {string} param - The get params.
+	 */
 	getRequestUrl: function (type, param) {
 		if (param) {
 			let url = 'https://' + this[type] + param;
