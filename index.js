aaaa14c554b7da1065bdae1a96cdf04f0ccfa01b
Vladislav Khvostov <vladkhvo@gmail.com>
2015-10-30 09:03:35 +0300

    Created the module about urls

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 let request = require('request');
+let requestUrls = require('./request-urls.js');
+
 
 // Скоп приложения.
 let bashlit = {};
@@ -12,28 +14,15 @@ bashlit.authKeys = {
 	"X-Client-ID": "94a0ac48561078fd5530",
 };
 
-// Урлы для запростов - https://developer.wunderlist.com/documentation
-bashlit.requestUrls = {
-	tasks: 'a.wunderlist.com/api/v1/tasks?list_id=',
-	lists: 'a.wunderlist.com/api/v1/lists',
-	getRequestUrl: function (type, param) {
-		if (param) {
-			let url = 'https://' + this[type] + param;
-			return url	
-		} else {
-			let url = 'https://' + this[type];
-			return url;
-		}
-	}
-}
 
-let listsUrl = bashlit.requestUrls.getRequestUrl('lists')
+
+let listsUrl = requestUrls.getRequestUrl('lists')
 
 // Тело запроса. like - https://a.wunderlist.com/api/v1/tasks?list_id=150029475
 let requesBody = {
  url: listsUrl,
  // Заголовки
- headers: bashlit.authKeys 
+ headers: bashlit.authKeys
 };
 
 
@@ -43,26 +32,26 @@ request(requesBody, (error, response, body) => {
 
  if (!error && response.statusCode == 200) {
 
- 	// Парсим ответ, получаем массив из объетов-списков
- let answerJSON = JSON.parse(body);
- let allListsId = [];
- let urlForReques = [];
+// Парсим ответ, получаем массив из объетов-списков
+let answerJSON = JSON.parse(body);
+let allListsId = [];
+let urlForReques = [];
 
 
 	let promise = new Promise((resolve, reject) => {
-	 // Получаем массив с идентификаторами от каждого списка.
-	 answerJSON.forEach((item, index, array) => {
-	 	allListsId.push(item.id);
-	 });
-	 resolve(allListsId);
+	// Получаем массив с идентификаторами от каждого списка.
+	answerJSON.forEach((item, index, array) => {
+		allListsId.push(item.id);
+	});
+	resolve(allListsId);
 	})
 
 	promise.then((allListsId) => {
 		let tasksRequests = [];
-		
+
 		allListsId.forEach(function (elem) {
-			// Для каждого идентификатора создаем ссылку-запрос.	
-			tasksRequests.push(bashlit.requestUrls.getRequestUrl('tasks', elem));
+			// Для каждого идентификатора создаем ссылку-запрос.
+			tasksRequests.push(requestUrls.getRequestUrl('tasks', elem));
 		})
 		return tasksRequests;
 	}).then((tasksRequests) => {
@@ -70,25 +59,19 @@ request(requesBody, (error, response, body) => {
 		let requesBody = {};
 		// Добавляем в тело зпроса штуки для авторизации.
 		requesBody.headers = bashlit.authKeys;
-		
+
 		tasksRequests.forEach((link) => {
-			 // Добавляем в тело зпроса урл
-			 requesBody.url = link;
-			 // Делам запрос для каждой ссылки.
-			 request(requesBody, (error, response, body) => {
+			// Добавляем в тело зпроса урл
+			requesBody.url = link;
+			// Делам запрос для каждой ссылки.
+			request(requesBody, (error, response, body) => {
 				let responseJSON = JSON.parse(body);
-			 	responseJSON.forEach((elem) => {
-			 		// Выводим в консоль все таски.
-			 		process.stdout.write(elem.title + '\n');
-			 	})
+					responseJSON.forEach((elem) => {
+					// Выводим в консоль все таски.
+					process.stdout.write(elem.title + '\n');
+				})
 			})
 		})
 	})
  }
 });
-
-
-
-
-
-// curl -H "X-Access-Token: 8d5596e05ed8de4c4eb5a9654fe5673c98b437f0d7cce23b730e97f0dbdb" -H "X-Client-ID: 94a0ac48561078fd5530" a.wunderlist.com/api/v1/lists
