6f6ce62840e20a577a5ca9dad2004551e20397d5
Vladislav Khvostov <vladkhvo@gmail.com>
2015-11-04 17:03:51 +0300

    Did some refactored

--- index.js
+++ index.js
@@ -11,60 +11,59 @@ let requesBody = {
  headers: authKeys
 };
 
-// Sent the reques and getting lists.
-request(requesBody, (error, response, body) => {
+let allListsId = new Promise((resolve, reject) => {
 
-	if (!error && response.statusCode == 200) {
+	request(requesBody, (error, response, body) => {
 
-		// Parse the answer, getting array from objects-lists.
-		let answerJSON = JSON.parse(body);
-		let allListsId = [];
-		let urlForReques = [];
-
-		let promise = new Promise((resolve, reject) => {
+		if (!error && response.statusCode == 200) {
+			// Parse the answer, getting array from objects-lists.
+			let answerJSON = JSON.parse(body);
+			let urlForReques = [];
+			let ids =[];
 
 			// Getting the array with ID from each list.
 			answerJSON.forEach((item) => {
-				return allListsId.push(item.id);
+				return ids.push(item.id);
 			});
 
-			resolve(Promise.all(allListsId));
-		});
-
-		promise.then((allListsId) => {
-			let tasksRequests = [];
-
-			allListsId.forEach(function (elem) {
-				// For each ID make a link-request.
-				tasksRequests.push(requestUrls.getRequestUrl('tasks', elem));
-			})
-
-			return Promise.all(tasksRequests);
-
-		}).then((tasksRequests) => {
-			let requesBody = {};
-
-			// Add in the request auth info.
-			requesBody.headers = authKeys;
-
-			tasksRequests.forEach((link) => {
-
-				// Add url in the request.
-				requesBody.url = link;
-
-				// Doing requst for all links.
-				request(requesBody, (error, response, body) => {
-					let responseJSON = JSON.parse(body);
-					let allTasks = [];
-
-					responseJSON.forEach((elem) => {
-						// Withdraw all the tasks in console.
-						process.stdout.write(elem.title + '\n');
-					})
-				})
-			})
-		}).catch(console.log.bind(console));
-	} else {
-		throw new Error(error);
-	}
+			resolve(Promise.all(ids));
+		} else {
+			throw new Error(error);
+		}
+	});
 });
+
+
+allListsId.then(function(ids){
+	let tasksRequests = [];
+
+	ids.forEach(function (id) {
+		// For each ID make a link-request.
+		tasksRequests.push(requestUrls.getRequestUrl('tasks', id));
+	})
+
+	return Promise.all(tasksRequests);
+
+}).then((tasksRequests)=>{
+	let requesBody = {};
+
+	// Add in the request auth info.
+	requesBody.headers = authKeys;
+
+	tasksRequests.forEach((link) => {
+
+		// Add url in the request.
+		requesBody.url = link;
+
+		// Doing requst for all links.
+		request(requesBody, (error, response, body) => {
+			let responseJSON = JSON.parse(body);
+			let allTasks = [];
+
+			responseJSON.forEach((elem) => {
+				// Withdraw all the tasks in console.
+				process.stdout.write(elem.title + '\n');
+			})
+		})
+	})
+}).catch(console.log.bind(console));
